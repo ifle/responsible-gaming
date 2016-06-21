02238aadbd6624bb9554379c01de648d8607749a
cesarc <cesarbmx@hotmail.com>
2016-06-21 13:32:51 +0200

    avoid ignoring null values in responses

--- Pinnacle.ResponsibleGaming.Events/DepositLimitSet.cs
+++ Pinnacle.ResponsibleGaming.Events/DepositLimitSet.cs
@@ -7,11 +7,8 @@ namespace Pinnacle.ResponsibleGaming.Events
     {
         public string CustomerId { get; set; }
         public decimal Amount { get; set; }
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int? PeriodInDays { get; set; }
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DateTime? StartDate { get; set; }
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DateTime? EndDate { get; set; }
         public string Author { get; set; }
         public DateTime CreationTime { get; set; }
