72a33bc9465afd23ff81b8ce836daa12b69e19b1
cesarc <cesarbmx@hotmail.com>
2016-07-05 18:18:15 +0200

    fix

--- Pinnacle.ResponsibleGaming.Domain/Models/DepositLimit.cs
+++ Pinnacle.ResponsibleGaming.Domain/Models/DepositLimit.cs
@@ -10,7 +10,7 @@ namespace Pinnacle.ResponsibleGaming.Domain.Models
 
         public void ApplyNewLimit(DepositLimit depositLimit)
         {
-            if (!DepositLimitRules.NewLimitMustBeMoreRestrictiveThanTheCurrentOne(depositLimit.Amount, Amount)) { throw new ConflictException(DepositLimitMessages.LimitMustBeMoreRestrictiveThanTheCurrentOne); }
+            if (!DepositLimitRules.NewLimitMustBeMoreRestrictiveThanTheCurrentOne(depositLimit.Amount, Amount)) { throw new ConflictException(DepositLimitMessages.PeriodAndLimitCannotBeChangedAtOnce); }
             if (!DepositLimitRules.PeriodAndLimitCannotBeChangedAtOnce(depositLimit.Amount, Amount, depositLimit.PeriodInDays, PeriodInDays)) { throw new ConflictException(DepositLimitMessages.LimitMustBeMoreRestrictiveThanTheCurrentOne); }          
             if (!DepositLimitRules.NewPeriodMustBeMoreRestrictiveThanTheCurrentOne(depositLimit.PeriodInDays, PeriodInDays)) { throw new ConflictException(DepositLimitMessages.PeriodMustBeMoreRestrictiveThanTheCurrentOne); }
             Map(depositLimit);
