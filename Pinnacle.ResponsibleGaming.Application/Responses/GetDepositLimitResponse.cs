be3cc18d8552224290584eac2dd496d03a287978
cesarc <cesarbmx@hotmail.com>
2016-07-09 16:02:29 +0200

    format response

--- Pinnacle.ResponsibleGaming.Application/Responses/GetDepositLimitResponse.cs
+++ Pinnacle.ResponsibleGaming.Application/Responses/GetDepositLimitResponse.cs
@@ -2,18 +2,30 @@
 
 namespace Pinnacle.ResponsibleGaming.Application.Responses
 {
-    public class GetDepositLimitResponse : DepositLimit
+    public class GetDepositLimitResponse 
     {
+        public string CustomerId { get; set; }
+        public decimal Amount { get; set; }
+        public LimitStatus Status { get; set; }
+        public bool IsRecurring { get; set; }
+        public string PeriodInDays { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Author { get; set; }
+        public string ModificationTime { get; set; }
+     
+
         public GetDepositLimitResponse(DepositLimit depositLimit)
-            :base(
-                 depositLimit.CustomerId,
-                 depositLimit.Amount,
-                 depositLimit.PeriodInDays,
-                 depositLimit.StartDate,
-                 depositLimit.EndDate,
-                 depositLimit.Author
-                 )
         {
+            CustomerId = depositLimit.CustomerId;
+            Amount = depositLimit.Amount;
+            Status = depositLimit.Status;
+            IsRecurring = depositLimit.IsRecurring;
+            PeriodInDays = depositLimit.PeriodInDays.ToString();
+            StartDate = depositLimit.StartDate.ToString("yyyy-MMM-dd HH:mm");
+            EndDate = depositLimit.EndDate?.ToString("yyyy-MMM-dd HH:mm");
+            Author = depositLimit.Author;
+            ModificationTime = depositLimit.ModificationTime.ToString("yyyy-MMM-dd HH:mm");
         }
     }
 }
