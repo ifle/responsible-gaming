c7b04a490c72686d49592ac5cead14f050d8246d
cesarc <cesarbmx@hotmail.com>
2016-07-09 16:07:52 +0200

    better response

--- Pinnacle.ResponsibleGaming.Application/Responses/GetDepositLimitResponse.cs
+++ Pinnacle.ResponsibleGaming.Application/Responses/GetDepositLimitResponse.cs
@@ -5,15 +5,14 @@ namespace Pinnacle.ResponsibleGaming.Application.Responses
     public class GetDepositLimitResponse : DepositLimit
     {
         public GetDepositLimitResponse(DepositLimit depositLimit)
-            :base(
-                 depositLimit.CustomerId,
-                 depositLimit.Amount,
-                 depositLimit.PeriodInDays,
-                 depositLimit.StartDate,
-                 depositLimit.EndDate,
-                 depositLimit.Author
-                 )
         {
+            CustomerId = depositLimit.CustomerId;
+            Amount = depositLimit.Amount;
+            PeriodInDays = depositLimit.PeriodInDays;
+            StartDate = depositLimit.StartDate;
+            EndDate = depositLimit.EndDate;
+            Author = depositLimit.Author;
+            ModificationTime = depositLimit.ModificationTime;
         }
     }
 }
