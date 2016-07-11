d311eb3786b3000ce695c9cdca134064247aa14c
cesarc <cesarbmx@hotmail.com>
2016-07-11 13:08:02 +0200

    refactor

--- Pinnacle.ResponsibleGaming.Domain/Services/EventService.cs
+++ Pinnacle.ResponsibleGaming.Domain/Services/EventService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Pinnacle.ResponsibleGaming.Domain.Models;
 using Pinnacle.ResponsibleGaming.Domain.Repositories;
 
