2473e2424174f57348545a46ca421dc048b15c4c
cesarc <cesarbmx@hotmail.com>
2016-07-14 18:35:04 +0200

    remove attribute

--- Pinnacle.ResponsibleGaming.Domain/Models/SelfExclusion.cs
+++ Pinnacle.ResponsibleGaming.Domain/Models/SelfExclusion.cs
@@ -1,11 +1,9 @@
-﻿using Newtonsoft.Json;
-
+﻿
 
 namespace Pinnacle.ResponsibleGaming.Domain.Models
 {
     public class SelfExclusion : Limit
     {
-        [JsonProperty(Order = 3)]
         public decimal TimeInDays { get; set; }
     }
 }
