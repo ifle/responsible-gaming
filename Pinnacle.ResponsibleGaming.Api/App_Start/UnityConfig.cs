d24aebbf11dd2eca8c0d804e61e01692c0d54f6c
cesarc <cesarbmx@hotmail.com>
2016-07-15 17:47:15 +0200

    rollback to yesterday (m)

--- Pinnacle.ResponsibleGaming.Api/App_Start/UnityConfig.cs
+++ Pinnacle.ResponsibleGaming.Api/App_Start/UnityConfig.cs
@@ -26,15 +26,13 @@ namespace Pinnacle.ResponsibleGaming.Api
 
             //Contexts
             container.RegisterType<Context, Context>(new HierarchicalLifetimeManager());
+            container.RegisterType<ISetDepositLimitContext, SetDepositLimitContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IDisableDepositLimitContext, DisableDepositLimitContext>(new HierarchicalLifetimeManager());
 
             //Handlers
             container.RegisterType<GetDepositLimitHandler, GetDepositLimitHandler>();
             container.RegisterType<SetDepositLimitHandler, SetDepositLimitHandler>();
             container.RegisterType<DisableDepositLimitHandler, DisableDepositLimitHandler>();
-
-            //Transactions
-            container.RegisterType<ISetDepositLimitContext, SetDepositLimitContext>();
-            container.RegisterType<IDisableDepositLimitContext, DisableDepositLimitContext>();
 
             //Services
             container.RegisterType<DepositLimitService, DepositLimitService>();
