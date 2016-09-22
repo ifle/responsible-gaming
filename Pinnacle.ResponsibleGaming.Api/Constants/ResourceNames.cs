6169ea3b018e4db59380ff6c73929f567f7aef8f
cesarc <cesarbmx@hotmail.com>
2016-09-22 06:11:35 -0700

    fix

--- Pinnacle.ResponsibleGaming.Api/Constants/ResourceNames.cs
+++ Pinnacle.ResponsibleGaming.Api/Constants/ResourceNames.cs
@@ -6,7 +6,7 @@ namespace Pinnacle.ResponsibleGaming.Api.Constants
     {
         public const string Customers = "customers"; //Customers with current limits   http://responsible-gaming.pinnacle.com/customer
         public const string Users = "users"; //Users that have set limits              http://responsible-gaming.pinnacle.com/users
-        public const string DepositLimit = "deposit-limit/{customerId}";               http://responsible-gaming.pinnacle.com/deposit-limit/testuser
+        public const string DepositLimit = "deposit-limit/{customerId}";//             http://responsible-gaming.pinnacle.com/deposit-limit/testuser
         public const string Log = "log"; //Log?{username}{customerId}                  http://responsible-gaming.pinnacle.com/log?customerId=testuser
     }
 }
