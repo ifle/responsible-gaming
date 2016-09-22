a27c44db818deb6e2b74918b7146e3ee2b3ca570
cesarc <cesarbmx@hotmail.com>
2016-09-22 06:09:20 -0700

    from stash

--- Pinnacle.ResponsibleGaming.Api/Constants/ResourceNames.cs
+++ Pinnacle.ResponsibleGaming.Api/Constants/ResourceNames.cs
@@ -4,9 +4,9 @@ namespace Pinnacle.ResponsibleGaming.Api.Constants
 {
     public static class ResourceNames
     {
-        public const string Customers = "customers"; //Customers with current limits
-        public const string Users = "users"; //Users that have set limits
-        public const string DepositLimit = "deposit-limit/{customerId}";
-        public const string Log = "log"; //Log?{username}{customerId}
+        public const string Customers = "customers"; //Customers with current limits   http://responsible-gaming.pinnacle.com/customer
+        public const string Users = "users"; //Users that have set limits              http://responsible-gaming.pinnacle.com/users
+        public const string DepositLimit = "deposit-limit/{customerId}";               http://responsible-gaming.pinnacle.com/deposit-limit/testuser
+        public const string Log = "log"; //Log?{username}{customerId}                  http://responsible-gaming.pinnacle.com/log?customerId=testuser
     }
 }
