68acdf81682e5ffd16595dc8560b0a8cdc2c46a4
cesarc <cesarc@pinnaclesports.com>
2016-10-25 08:10:23 -0700

    Add Orleans projects

---
+++ Pinnacle.ResponsibleGaming.Grains/Grain1.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Orleans;
+using Pinnacle.ResponsibleGaming.IGrains;
+
+namespace Pinnacle.ResponsibleGaming.Grains
+{
+    /// <summary>
+    /// Grain implementation class Grain1.
+    /// </summary>
+    public class Grain1 : Grain, IGrain1
+    {
+
+    }
+}
