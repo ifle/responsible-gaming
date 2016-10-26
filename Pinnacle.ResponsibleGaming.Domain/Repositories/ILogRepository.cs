c277c65e5bad64d5c2d3ec38b0c71edd44121b97
cesarc <cesarc@pinnaclesports.com>
2016-10-26 04:46:26 -0700

    refactor

--- Pinnacle.ResponsibleGaming.Domain/Repositories/ILogRepository.cs
+++ Pinnacle.ResponsibleGaming.Domain/Repositories/ILogRepository.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Pinnacle.ResponsibleGaming.Domain.Entities;
+﻿using Pinnacle.ResponsibleGaming.Domain.Entities;
 
 namespace Pinnacle.ResponsibleGaming.Domain.Repositories
 {
